1c4fefd2e08676cf1e07390d2e42515498643531
taoky <me@taoky.moe>
2023-12-04 03:30:53 +0800

    Add stream mode support

--- src/main.rs
+++ src/main.rs
@@ -19,66 +19,41 @@ struct Char {
 
 #[derive(Parser)]
 struct Args {
+    /// Show black background instead of white
     #[clap(short, long)]
     inverted: bool,
 
+    /// Stream mode. Takes input from stdin and prints it as it comes.
+    #[clap(short, long)]
+    stream: bool,
+
+    /// Words to print. In stream mode, this is ignored.
+    #[clap(default_value = "")]
     words: String,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    let term_col = match termsize::get() {
-        None => 80,
-        Some(s) => s.cols,
-    };
-    if term_col < 16 {
-        println!("terminal width must be at least 16");
-        exit(1);
-    }
-
-    let hexfile = "./unifont_all.hex";
-    let file = File::open(hexfile).unwrap();
-    let mut mapping = HashMap::new();
-    for line in BufReader::new(file).lines() {
-        // split by :
-        let line = line.unwrap();
-        if line.trim().is_empty() {
-            continue;
+macro_rules! get_term_col {
+    () => {
+        match termsize::get() {
+            None => 80,
+            Some(s) => {
+                if s.cols < 16 {
+                    println!("terminal width must be at least 16");
+                    exit(1);
+                }
+                s.cols
+            }
         }
-        let (code, data) = line.split_once(':').unwrap();
-        let code = u32::from_str_radix(code, 16).unwrap();
-        let mut line_vec = vec![];
-        let width = if data.len() == 64 {
-            // push to line_vec every 4 chars
-            for i in 0..16 {
-                let byte = &data[i * 4..i * 4 + 4];
-                let byte = u16::from_str_radix(byte, 16).unwrap();
-                line_vec.push(byte);
-            }
-            CharWidth::Width16
-        } else if data.len() == 32 {
-            // push to line_vec every 2 chars
-            for i in 0..16 {
-                let byte = &data[i * 2..i * 2 + 2];
-                let byte = u8::from_str_radix(byte, 16).unwrap();
-                line_vec.push(byte as u16);
-            }
-            CharWidth::Width8
-        } else {
-            panic!("invalid data length: {}", data.len());
-        };
-        mapping.insert(
-            code,
-            Char {
-                width,
-                data: line_vec,
-            },
-        );
-    }
+    };
+}
 
+fn push_line(words: &[char], mapping: &HashMap<u32, Char>, args: &Args, initial_term_col: u16) {
+    let term_col = if args.stream {
+        get_term_col!()
+    } else {
+        initial_term_col
+    };
     let mut start = 0;
-    let words = args.words.chars().collect::<Vec<_>>();
     while start < words.len() {
         let mut end = start;
         let mut width = 0;
@@ -120,13 +95,71 @@ fn main() {
                     format!("{:016b}", line)
                 };
                 if args.inverted {
-                    print!("{}", line.replace("0", "█").replace("1", " "));
+                    print!("{}", line.replace('0', "█").replace('1', " "));
                 } else {
-                    print!("{}", line.replace("0", " ").replace("1", "█"));
+                    print!("{}", line.replace('0', " ").replace('1', "█"));
                 }
             }
             println!();
         }
         start = end;
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let initial_term_col = get_term_col!();
+
+    let hexfile = "./unifont_all.hex";
+    let file = File::open(hexfile).unwrap();
+    let mut mapping = HashMap::new();
+    for line in BufReader::new(file).lines() {
+        // split by :
+        let line = line.unwrap();
+        if line.trim().is_empty() {
+            continue;
+        }
+        let (code, data) = line.split_once(':').unwrap();
+        let code = u32::from_str_radix(code, 16).unwrap();
+        let mut line_vec = vec![];
+        let width = if data.len() == 64 {
+            // push to line_vec every 4 chars
+            for i in 0..16 {
+                let byte = &data[i * 4..i * 4 + 4];
+                let byte = u16::from_str_radix(byte, 16).unwrap();
+                line_vec.push(byte);
+            }
+            CharWidth::Width16
+        } else if data.len() == 32 {
+            // push to line_vec every 2 chars
+            for i in 0..16 {
+                let byte = &data[i * 2..i * 2 + 2];
+                let byte = u8::from_str_radix(byte, 16).unwrap();
+                line_vec.push(byte as u16);
+            }
+            CharWidth::Width8
+        } else {
+            panic!("invalid data length: {}", data.len());
+        };
+        mapping.insert(
+            code,
+            Char {
+                width,
+                data: line_vec,
+            },
+        );
+    }
+
+    if !args.stream {
+        let words = args.words.chars().collect::<Vec<_>>();
+        push_line(&words, &mapping, &args, initial_term_col);
+    } else {
+        let stdin = std::io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line.unwrap();
+            let words = line.chars().collect::<Vec<_>>();
+            push_line(&words, &mapping, &args, initial_term_col);
+        }
     }
 }
